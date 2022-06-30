314630439a2e7d55eaf959eca28e7bbe924060df
Luiz Felipe Limao <limao@limao.com>
2022-06-29 23:22:06 -0300

    added: secrets and envs

--- server.go
+++ server.go
@@ -1,15 +1,47 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	app := gin.Default()
 	router := app.Group("/")
+
 	router.GET("/", Hello)
+	router.GET("/configmap", configMap)
+	router.GET("/secret", secret)
+
 	app.Run(":8080")
 
 }
 
 func Hello(c *gin.Context) {
-	c.Writer.Write([]byte("<h1> hello k8s </h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(c.Writer, "Hello i am %s. I am %s", name, age)
+}
+
+func configMap(c *gin.Context) {
+
+	data, err := ioutil.ReadFile("/usr/src/app/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error:", err)
+	}
+
+	fmt.Fprintf(c.Writer, "My family %s", string(data))
+
+}
+
+func secret(c *gin.Context) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(c.Writer, "user %s and password %s", user, password)
 }
