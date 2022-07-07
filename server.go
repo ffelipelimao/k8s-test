a3bdfae1121cb9af9b870ae562dae2f4bf1fc491
Luiz Felipe Limao <lflimao@hotmail.com>
2022-07-06 22:42:21 -0300

    feature: added liveness, readiness n startup probe

--- server.go
+++ server.go
@@ -5,15 +5,19 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var startedAt = time.Now()
 
 func main() {
 	app := gin.Default()
 	router := app.Group("/")
 
 	router.GET("/", Hello)
+	router.GET("/healthz", Healthz)
 	router.GET("/configmap", configMap)
 	router.GET("/secret", secret)
 
@@ -26,6 +30,17 @@ func Hello(c *gin.Context) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(c.Writer, "Hello i am %s. I am %s", name, age)
+}
+
+func Healthz(c *gin.Context) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+		c.Writer.WriteHeader(500)
+		c.Writer.Write([]byte(fmt.Sprintf("Duration %s", duration)))
+	} else {
+		c.Writer.WriteHeader(200)
+		c.Writer.Write([]byte("ok"))
+	}
 }
 
 func configMap(c *gin.Context) {
